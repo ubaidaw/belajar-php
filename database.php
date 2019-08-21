02b5112ebaceca85c4d6795189cc91c79d242363
ubaidaw <54278504+ubaidaw@users.noreply.github.com>
2019-08-21 19:57:29 +0700

    Add files via upload

---
+++ database.php
@@ -0,0 +1,6 @@
+<?php 
+
+	echo "konfigurasi database....";
+
+
+ ?>
---
+++ include-require.php
@@ -0,0 +1,13 @@
+<?php 
+
+	// include ("config/database2.php");
+
+	// require ("config/database2.php");	
+
+	include_once("config/database.php");
+	include_once("config/database.php");
+	include_once("config/database.php");
+
+	echo " ini program saya";
+
+ ?>
